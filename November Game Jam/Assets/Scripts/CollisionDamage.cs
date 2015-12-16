613cb4f764d58e739da2ed996b649480b011a81f
CaptKonrad <robkondratowicz@gmail.com>
2015-12-16 00:39:19 -0600

    Adds Collision Damage
    This closes #4

---
+++ November Game Jam/Assets/Scripts/CollisionDamage.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using System.Collections;
+
+public class CollisionDamage : MonoBehaviour
+{
+	public float VelocityThreshold = 30;
+	public float DamageMultiplier = 1;
+	
+	private Rigidbody2D thisrb;
+	private Health hp;
+
+	// Use this for initialization
+	void Start ()
+	{
+		thisrb = gameObject.GetComponent<Rigidbody2D>();
+		hp = gameObject.GetComponent<Health>();
+	}
+	
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if(other.gameObject.tag != "Bullet")
+		{
+			float dam = (other.relativeVelocity.magnitude - VelocityThreshold) * DamageMultiplier;				
+			if(dam > 0)
+			{
+				if(hp != null)
+				{
+					hp.damage ((int)dam);
+				}
+				else
+				{
+					gameObject.SetActive(false);
+				}
+			}
+		}
+	}
+}
